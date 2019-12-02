7a8b6d3d60cd97e710aead2befa51d3ae93a9417
Yla Buri <31744347+YlaBuri@users.noreply.github.com>
2019-12-02 18:46:44 -0300

    Update consultas.sql

--- ProjetoRoteiro/src/main/resources/banco/consultas.sql
+++ ProjetoRoteiro/src/main/resources/banco/consultas.sql
@@ -22,3 +22,34 @@ on pontos.id=  rp.id_ponto and roteiros.id = rp.id_roteiro;
 SELECT roteiros.codigo, pontos.descricao FROM pontos inner join roteiros inner join roteiro_ponto as rp
 on pontos.id=  rp.id_ponto and roteiros.id = rp.id_roteiro
 where roteiros.id=1;
+
+-- AGREGAÇÃO 
+	-- MAX
+	select max(nome) from usuarios;
+    
+    -- MIN
+    select min(nome) from motoristas;
+    
+    -- SUM
+     select sum(duracao) from cursos;
+    
+    -- COUNT 
+    select count(*) from cursos where id_instituicao=2;
+    
+    -- AVG
+	select avg(duracao) from cursos;
+    
+-- AGRUPAMENTO
+	-- GROUP BY
+    select cursos.nome, count(*) from cursos 
+    group by cursos.nome WITH ROLLUP ;
+    
+-- DATA
+	select * from programacao where dia='2019-12-02';
+
+	select * from programacao where dia between '2019-11-01' and '2019-11-30';
+    
+-- AGRUPAMENTO E FILTRO POR GRUPO
+
+	select cursos.duracao, count(*) from cursos 
+    group by cursos.duracao having max(duracao) > 3;
